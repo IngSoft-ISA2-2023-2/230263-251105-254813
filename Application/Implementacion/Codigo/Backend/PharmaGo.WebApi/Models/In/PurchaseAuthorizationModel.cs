c6b1a9fc61f6eb136da83b6c69cd2936c26323dc
julietasarantes1 <julietasarantesmachin@gmail.com>
2023-09-18 23:46:13 -0300

    Subiendo entrega uno

--- Application/Implementacion/Codigo/Backend/PharmaGo.WebApi/Models/In/PurchaseAuthorizationModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PharmaGo.WebApi.Models.In
-{
-    public class PurchaseAuthorizationModel
-    {
-        public int pharmacyId { get; set; }
-        public string drugCode { get; set; }
-
-    }
-}
