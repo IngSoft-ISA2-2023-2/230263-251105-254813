c0bfe2b6184ca997d92e4c85057c1b4ec667be5d
julietasarantes1 <julietasarantesmachin@gmail.com>
2023-10-26 15:08:58 -0300

    Codigo invalido con specflow

--- Codigo/Backend/SpecFlowProject2/StepDefinitions/AgregarProductoStepDefinitions.cs
+++ Codigo/Backend/SpecFlowProject2/StepDefinitions/AgregarProductoStepDefinitions.cs
@@ -82,6 +82,11 @@ namespace SpecFlowPharmaGo.WebApi.StepDefinitions
                 _exception = ex;
             }
         }
+        [Then(@"salta un mensaje de error con codigo invalido y el producto no se agrega a la lista")]
+        public void ThenSaltaUnMensajeDeErrorConCodigoInvalidoYElProductoNoSeAgregaALaLista()
+        {
+            Assert.AreEqual(500, (int)context.Get<HttpStatusCode>("ResponseStatusCode"));
+        }
 
         [Then(@"agrega el producto a la farmacia")]
         public void ThenAddTheProductToPharmacy()
@@ -96,17 +101,7 @@ namespace SpecFlowPharmaGo.WebApi.StepDefinitions
             }
         }
 
-        [Then(@"salta un mensaje de error con nombre invalido y el producto no se agrega a la lista")]
-        public void ThenTheProductNotAddToPharmacy()
-        {
-            if (_exception == null)
-            {
-                Assert.AreEqual(200, (int)context.Get<HttpStatusCode>("ResponseStatusCode"));
-            }
-            else
-            {
-                Assert.IsTrue(true);
-            }
-        }
+        
+
     }
 }
